53bd900acb3bb0500bb5c866ff69e781edb8d676
Shai Pilosof <shainova@gmail.com>
2021-04-04 18:32:25 +0300

    Added comparison between observed and simulated data

--- empirical_data.R
+++ empirical_data.R
@@ -24,8 +24,7 @@ R0_country <- function(the_country,...){
   R0
 }
 # Get data
-setwd('~/GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/')
-covid19 <- read_csv('time_series_covid19_confirmed_global.csv')
+covid19 <- read_csv('~/GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 covid19 %<>%
   dplyr::select(-`Province/State`, -Lat, -Long) %>%
   rename(country=`Country/Region`) %>%
@@ -51,22 +50,53 @@ R0 <- est.R0.EG(y, mGT, begin=1, end=early_period_limit)
 R0
 beta_est_R0 <- R0$R*(1/7) # beta=R0*gamma
 
-# Estimate beta using a linear model
-
 ildata_early <- ildata %>% filter(running_day<=early_period_limit)
-model_lm <- lm(log(cases) ~ running_day, data=ildata_early) 
-s <- summary(model_lm)
-beta_gamma_coef <- coef(model_lm)[2]
 
+# Estimate beta using a linear model
+# 
+# model_lm <- lm(log(cases) ~ running_day, data=ildata_early) 
+# s <- summary(model_lm)
+# beta_gamma_coef <- coef(model_lm)[2]
 # The coefficient is actually beta-gamma so calculate beta:
-beta <- beta_gamma_coef+gamma
+# beta <- beta_gamma_coef+gamma
 
 # Check the fit of the model by plottign predicted values based on model estimation
-ildata_early$pred_lm <- exp(predict(model_lm))
-ggplot(ildata_early, aes(y=cases, x=running_day))+
-  geom_point()+geom_line()+
-  geom_line(aes(x = running_day, y = pred_lm), color='blue', size=1.3)+
-  theme_bw()
+# ildata_early$pred_lm <- exp(predict(model_lm))
+# ggplot(ildata_early, aes(y=cases, x=running_day))+
+#   geom_point()+geom_line()+
+#   geom_line(aes(x = running_day, y = pred_lm), color='blue', size=1.3)+
+#   theme_bw()
+
+# ggplot(ildata_early, aes(y=cases, x=running_day))+
+#   geom_point()+geom_line()+
+#   geom_line(aes(x = running_day, y = pred_lm), color='blue', size=1.3)+
+#   theme_bw()
+
+d <- read_csv('100_results_Israel.csv') # This simulation was run manually.
+pdf('/Users/shai/Dropbox (BGU)/Apps/Overleaf/COVID19/PLOS Comp/Revision/beta_fitting.pdf', 6, 6)
+d %>% 
+  filter(SD==0, k_percent==0, from=='juveniles_adults_elderly', vto=='elderly_adults') %>% 
+  filter(time<=35) %>%
+  filter(state == 'I') %>%
+  group_by(time, state) %>% 
+  summarise(cases=sum(cases)) %>% 
+  dplyr::select(running_day=time, cases) %>% 
+  mutate(Source='Model') %>% 
+  bind_rows(ildata_early %>% ungroup() %>%  dplyr::select(running_day, cases) %>%  mutate(Source='Data')) %>% 
+  ggplot(aes(running_day, cases, color=Source))+
+  geom_line(size=1.1)+
+  geom_point(size=3)+
+  scale_color_manual(values=c('purple','black'))+
+  scale_x_continuous(n.breaks = 6)+
+  scale_y_continuous(n.breaks = 6)+
+  theme_bw()+
+  labs(x='Day', y='Infected cases')+
+  theme(panel.grid = element_blank(),
+        legend.position = c(0.2,0.8),
+        axis.title = element_text(size=14, color = 'black'), 
+        axis.text = element_text(size=14, color = 'black'))
+dev.off()
+
 
 
 
